84687edd4782bbf1345773fb0bd7aa26c89d8ad1
Reyna31 <53918885+Reyna31@users.noreply.github.com>
2020-02-06 00:25:34 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        iCalculadora Calculadora = new Micalculadora(); // Se instancia una nueva calculadora
+        IStack<Integer> CStack = new MiStack<Integer>();// Se instancia un Stack
+        ICalculator Calculadora = new Calculator(); // Se instancia una nueva calculadora
         Scanner sc = null; // Se instancia un scanner que servira para leer el archivo de texto
 
         String archivo ="dato.txt"; //Se crea un String el cual servira para poder leer el archivo de texto
-        File fl = new fichero(archivo);
+        File fl = new File(archivo);
+        String datos = "";
 
         //Se crea un try and catch por posibles errores
         try {
@@ -18,7 +22,36 @@ public class Main {
             ex.printStackTrace();
         }
 
+        String[] datosList = datos.split("\\s");// Guarda los datos en un array con split
+        int contador = 0;//Variable que me servira para poder saber que dato se esta leyendo
 
+        while (contador < datosList.length ){
+            CStack.push(Integer.parseInt(datosList[contador]));
+            if (CStack.size() < 2){
+                System.out.println("No hay suficientes datos para operar");
+            }
+            else {
+                if (datosList.equals("+")){
+                    CStack.push(Calculadora.sum(CStack.pop(),CStack.pop()));
+                }else if (datosList.equals("-")){
+                    CStack.push(Calculadora.subtraction(CStack.pop(),CStack.pop()));
+                }else if (datosList.equals("*")){
+                    CStack.push(Calculadora.multiply(CStack.pop(),CStack.pop()));
+                }else if (datosList.equals("/")){
+                    CStack.push(Calculadora.division(CStack.pop(),CStack.pop()));
+                }else{
+                    System.out.println("No se puede realizar la operacion porfavor ingrese nuevamente");
+                }
+
+            }
+            contador++;// aumenta el contador
+        }
+
+        if (CStack.size() == 1){
+            System.out.println("Su resultado es: "+ CStack.get());//imprime el resultado de las operaciones
+        }else{
+            System.out.println("No se pudo procesar la informacion porfavor comprueba los datos.");// en caso de que no se puede leer el archivo de texto
+        }
     }
 }
 
