a3985fa962fbe74cae5336354816ebc71d0cbae4
Reyna31 <53918885+Reyna31@users.noreply.github.com>
2020-02-06 20:11:51 -0600

    Add files via upload

---
+++ ICalculator.java
@@ -0,0 +1,12 @@
+
+public interface ICalculator {
+    // realiza suma de dos parametros
+    int sumar(int a, int b);
+    // relaiza la resta de dos parametros
+    int restar(int a, int b);
+    // realiza la division de dos parametros
+    int division(int a, int b);
+    // realiza la multiplicacion de dos parametros
+    int multiplicacion(int a, int b);
+
+}
